4e999dd06b10d5d1bd46c1a61e025eaada1c0a5d
“Mayank <mayanksrivastava11111@gmail.com>
2026-01-17 01:12:49 +0530

    Initial project setup with backend and mongoDB connection

---
+++ backend/src/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+
+function connectDB(){
+    mongoose.connect("mongodb://127.0.0.1:27017/FlavourFeed")
+.then(()=>{
+        console.log("MongoDB Connected");
+    })
+    .catch((err)=>{
+        console.log("MongoDB connection error:",err);
+    });
+}
+
+module.exports = connectDB;
